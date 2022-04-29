35cbf0bce8b451d686fe41eb2884e2c0f9210c3d
Umar Ahmed <39865635+UmarGit@users.noreply.github.com>
2022-04-29 20:11:48 +0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import {BrowserRouter as  Router} from "react-router-dom";
 import {AppProvider} from './services/context'
 import './index.css';
 import App from './App';
-import {requestPermission} from './push-notification'
+// import {requestPermission} from './push-notification'
 import registerServiceWorker from "./serviceWorker";
 
 ReactDOM.render(
